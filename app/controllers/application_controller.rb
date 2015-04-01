eaf3d4f8f607721c4fa995c983e8677a568bc204
rhnorment <norment@gmail.com>
2015-04-01 11:45:23 -0500

    application layout complete with global variables.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
       @company_name = 'Rivendell Program'
       @phone_number = '952-996-1451'
       @inquiry_email = 'inquiries@rivendellsanctuary.com'
+      @sdc_url = 'http://sdcc.edu'
+      @wasc_url = 'http://www.wascsenior.org'
       @twitter_url = 'https://twitter.com/RivendellSanct'
       @facebook_url = 'https://www.facebook.com/rivendellsanctuary'
       @pinterest_url = 'https://www.pinterest.com/rivendellsanct/'
