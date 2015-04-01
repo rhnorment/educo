778b6ee187d12786abba5d482e8ebe6324ee6fab
rhnorment <norment@gmail.com>
2015-04-01 12:31:39 -0500

    errors controller and views complete and specs pass.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,21 @@
 class ErrorsController < ApplicationController
 
+  layout 'errors'
+
   def not_found
+    @page_title = 'Page not found.'
+    @message = 'You must have accidentally tried to access a page that does not exist on our site.
+                Click OK to return to the previous page.'
   end
 
   def unprocessable
+    @page_title = 'Unprocessable entity.'
+    @message = 'We cannot process this entity.  Click OK to return to the previous page.'
   end
 
   def server_error
+    @page_title = 'Internal server error.'
+    @message = 'Monkeys have invaded our servers.  Click OK to return to the previous page.'
   end
+
 end
--- spec/features/handle_exceptions_spec.rb
+++ spec/features/handle_exceptions_spec.rb
@@ -2,17 +2,14 @@ require 'spec_helper'
 
 describe 'handing exceptions' do
 
-  context 'when routing to an unavailable page' do
+  context 'when requesting an unavailable page' do
 
     it 'should present the page not found error page when visiting an unavailable route' do
-      visit root_url
       visit '/foo'
 
-
-
+      expect(page).to have_text('Page not found.')
+      expect(page).to have_link('OK')
     end
-
-    it 'should return the user to the original URI when the user clicks the OK button'
 
   end
 
