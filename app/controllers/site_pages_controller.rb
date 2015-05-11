913218d6b778ff0f982f01e698709e927f6158f4
Hunt Norment <hunt@sherpa.red>
2015-05-11 12:46:23 -0500

    site skeleton complete.  navigation complete.  all specs pass.

--- app/controllers/site_pages_controller.rb
+++ app/controllers/site_pages_controller.rb
@@ -5,5 +5,21 @@ class SitePagesController < ApplicationController
   end
 
   def about
+    @body_id = 'about-us'
   end
+
+  def academics
+    @body_id = 'features'
+  end
+
+  def student_life
+    @body_id = 'services'
+  end
+
+  def admissions
+  end
+
+  def apply_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root          'site_pages#home'
 
   get           '/about',           to: 'site_pages#about'
+  get           '/academics',       to: 'site_pages#academics'
+  get           '/student_life',    to: 'site_pages#student_life'
+  get           '/admissions',      to: 'site_pages#admissions'
+  get           '/apply_now',       to: 'site_pages#apply_now'
 
   get           '/student',         to: 'landing_pages#student'
   get           '/parent',          to: 'landing_pages#parent'
