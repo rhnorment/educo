4c21336c1303a129e72f1df59af3207c0c057800
Hunt Norment <hunt@sherpa.red>
2015-05-13 11:30:36 -0500

    removed session[:tag_id] from landing_pages controller.

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -3,14 +3,9 @@ class LandingPagesController < ApplicationController
   layout 'landing'
 
   def student
-    session[:tag_id] = 271
-
-    @lead = Lead.new
   end
 
   def parent
-    session[:tag_id] = 271
-
-    @lead = Lead.new
   end
+  
 end
