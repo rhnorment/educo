c9c2d4cc33393bdf7316cc649f4c394d4f31f7b8
Hunt Norment <hunt@sherpa.red>
2015-06-13 12:04:57 -0500

    added Infusionsoft concern as mixin to various models.

--- app/models/concerns/infusionsoftable.rb
+++ app/models/concerns/infusionsoftable.rb
@@ -1,6 +1,26 @@
 module Infusionsoftable
 
-  
+  extend ActiveSupport::Concern
 
+  def add_contact_to_infusionsoft
+    contact_id = Infusionsoft.contact_add_with_dup_check( { FirstName: self.first_name,
+                                                            LastName: self.last_name,
+                                                            Email: self.email, Phone1: self.phone, LeadSourceId: self.leadsource_id },
+                                                          'EmailAndName'
+    )
+
+    email_optin(self.email)
+
+    add_contact_to_group(contact_id, self.trigger_tag_id) if self.trigger_tag_id.present?
+    add_contact_to_group(contact_id, ADD_TO_CALL_LIST_ID) if self.phone.present?
+  end
+
+  def email_optin(email)
+    Infusionsoft.email_optin(email, 'completed form')
+  end
+
+  def add_contact_to_group(contact_id, tag_id)
+    Infusionsoft.contact_add_to_group(contact_id, tag_id)
+  end
 
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -15,6 +15,9 @@
 
 class Lead < ActiveRecord::Base
 
+  # configuration
+  include Infusionsoftable
+
   # constants
   TRIGGER_TAG_ID = 271
   ADD_TO_CALL_LIST_ID = 293
@@ -27,26 +30,5 @@ class Lead < ActiveRecord::Base
 
   # callbacks:
   after_create    :add_contact_to_infusionsoft
-
-  def add_contact_to_infusionsoft
-    contact_id = Infusionsoft.contact_add_with_dup_check( { FirstName: self.first_name,
-                                                LastName: self.last_name,
-                                                Email: self.email, Phone1: self.phone, LeadSourceId: self.leadsource_id },
-                                                'EmailAndName'
-    )
-
-    email_optin(self.email)
-
-    add_contact_to_group(contact_id, self.trigger_tag_id) if self.trigger_tag_id.present?
-    add_contact_to_group(contact_id, ADD_TO_CALL_LIST_ID) if self.phone.present?
-  end
-
-  def email_optin(email)
-    Infusionsoft.email_optin(email, 'completed form')
-  end
-
-  def add_contact_to_group(contact_id, tag_id)
-    Infusionsoft.contact_add_to_group(contact_id, tag_id)
-  end
 
 end
