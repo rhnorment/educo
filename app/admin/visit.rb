bdc0bad224423ceaa84a9f53dca265ed38276834
Hunt Norment <hunt@sherpa.red>
2015-06-13 08:21:57 -0500

    added active admin view for visits.

---
+++ app/admin/visit.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: visits
+#
+#  id            :integer          not null, primary key
+#  student_name  :string
+#  parent_name   :string
+#  student_phone :string
+#  parent_phone  :string
+#  student_email :string
+#  parent_email  :string
+#  student_dob   :date
+#  parent_dob    :date
+#  travel_dates  :text
+#  travel_times  :text
+#  airport       :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+ActiveAdmin.register Visit do
+
+  menu priority: 3
+
+  index do
+    column  :student_name
+    column  :parent_name
+    column  :travel_dates
+    column  :travel_times
+    column  :airport
+  end
+
+end
