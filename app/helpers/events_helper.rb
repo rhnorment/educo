c114844994b6836d5d3636d812773d1ae0baffdf
Hunt Norment <hunt@sherpa.red>
2015-06-19 10:08:42 -0500

    added events address helper.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+
+  def address_for(event)
+    content = event.address1
+    content << '<br />' + event.address2 if event.address2.present?
+    sanitize(content)
+  end
+
 end
+
+
